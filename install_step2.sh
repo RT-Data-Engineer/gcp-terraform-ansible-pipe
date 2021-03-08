7b566cea087f038e21a9fadacacab27ee1d65083
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-03-08 16:19:43 +0300

    Update install_step2.sh

--- install_step2.sh
+++ install_step2.sh
@@ -1,3 +1,4 @@
 id=$(gcloud projects list --sort-by ~PROJECT_ID | grep rt- | head -n 1 | awk '{print $1;}'
+gcloud config set project $id
 gsutil mb gs://$id
 gcloud beta dataproc clusters create cluster-243c --enable-component-gateway --bucket $id --region us-central1 --zone us-central1-a --single-node --master-machine-type n1-standard-2 --master-boot-disk-size 40 --image-version 2.0-debian10 --optional-components HIVE_WEBHCAT,ZEPPELIN --project $id
