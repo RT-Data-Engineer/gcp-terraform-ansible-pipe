8cee3347dcd954134c04469bc04a9ee9fe8d9af0
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-03-08 16:21:26 +0300

    Update install_step2.sh

--- install_step2.sh
+++ install_step2.sh
@@ -1,4 +1,4 @@
-id=$(gcloud projects list --sort-by ~PROJECT_ID | grep rt- | head -n 1 | awk '{print $1;}'
+id=$(gcloud projects list --sort-by ~PROJECT_ID | grep rt- | head -n 1 | awk '{print $1;}')
 
 gcloud config set project $id
 
