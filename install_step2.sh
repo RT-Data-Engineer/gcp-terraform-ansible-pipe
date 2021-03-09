ee6bab510e7f387e134c5f804ca1c956aefb23b1
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-03-09 23:45:57 +0300

    Update install_step2.sh

--- install_step2.sh
+++ install_step2.sh
@@ -4,6 +4,6 @@ gcloud config set project $id
 
 gcloud services enable dataproc.googleapis.com
 
-gsutil mb gs://$id
+gsutil mb gs://$id-2
 
-gcloud beta dataproc clusters create cluster-243c --enable-component-gateway --bucket $id --region us-central1 --zone us-central1-a --single-node --master-machine-type n1-standard-2 --master-boot-disk-size 40 --image-version 2.0-debian10 --optional-components HIVE_WEBHCAT,ZEPPELIN --project $id
+gcloud beta dataproc clusters create cluster-243c --enable-component-gateway --bucket $id-2 --region us-central1 --zone us-central1-a --single-node --master-machine-type n1-standard-2 --master-boot-disk-size 40 --image-version 2.0-debian10 --optional-components HIVE_WEBHCAT,ZEPPELIN --project $id
