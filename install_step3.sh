aee9058b6b8a60a5761bbf1f172a584540cb475c
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-03-15 12:17:57 +0300

    Update install_step3.sh

--- install_step3.sh
+++ install_step3.sh
@@ -1,3 +1,5 @@
 id=$(gcloud projects list --sort-by ~PROJECT_ID | grep rt- | head -n 1 | awk '{print $1;}')
 
 gcloud config set project $id
+
+cd $HOME/bootstrap/geenplum && terraform init && terraform apply -var-file=$HOME/bootstrap/gcp.tfvars -auto-approve
