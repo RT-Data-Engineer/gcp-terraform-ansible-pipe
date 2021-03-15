af9d47f19a2e51af57bf51c8fa7298cd4ddb0e77
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-03-15 12:35:13 +0300

    Update install_step3.sh

--- install_step3.sh
+++ install_step3.sh
@@ -3,7 +3,7 @@ id=$(gcloud projects list --sort-by ~PROJECT_ID | grep rt- | head -n 1 | awk '{p
 gcloud config set project $id
 
 sed -i -e "s/@bucket-name/\"$id\"/g" greenplum/create-instances.tf
-cd $HOME/step3/geenplum && terraform init && terraform apply -var-file=$HOME/bootstrap/gcp.tfvars -auto-approve
+cd $HOME/step3/greenplum && terraform init && terraform apply -var-file=$HOME/bootstrap/gcp.tfvars -auto-approve
 
 cd $HOME/bootstrap/compute && terraform refresh -var-file=$HOME/bootstrap/gcp.tfvars
 cd $HOME/bootstrap/template && terraform init && terraform destroy -var-file=$HOME/bootstrap/gcp.tfvars -auto-approve
