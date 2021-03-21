147e32a6832eb972563f42a289683e34be57360b
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-03-21 21:40:39 +0300

    Update generate_svc.sh

--- generate_svc.sh
+++ generate_svc.sh
@@ -4,7 +4,7 @@ gcloud iam service-accounts create svc-$student \
 
 gcloud projects add-iam-policy-binding $student \
     --member="serviceAccount:svc-$student@$id.iam.gserviceaccount.com" \
-    --role="roles/owner"
+    --role="roles/viewer"
 
 gcloud iam service-accounts keys create $HOME/$student-credentials.json \
   --iam-account svc-$student@$id.iam.gserviceaccount.com
