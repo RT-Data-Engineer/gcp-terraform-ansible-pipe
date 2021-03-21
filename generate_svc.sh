88fde8c71397044d5355355073f6b321f953d2e5
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-03-21 21:43:22 +0300

    Update generate_svc.sh

--- generate_svc.sh
+++ generate_svc.sh
@@ -1,3 +1,14 @@
+students=(
+  student1
+  student2
+  student3
+)
+
+id=projectid
+
+
+for student in "${students[@]}"; do
+    
 gcloud iam service-accounts create svc-$student \
     --description="$student-Service-Account" \
     --display-name="$student-ServiceAccount"
@@ -12,3 +23,5 @@ gcloud iam service-accounts keys create $HOME/$student-credentials.json \
 gsutil mb gs://$id-$student
 
 gsutil iam ch serviceAccount:svc-$student@$id.iam.gserviceaccount.com:objectAdmin gs://$id-$student
+
+done
