e5a08f97f6a5ea051b156aee12d3a34d83fbba0d
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-03-26 11:37:19 +0300

    Update generate_pwd.sh

--- generate_pwd.sh
+++ generate_pwd.sh
@@ -1,11 +1,11 @@
 students=(
-  student6
-  student2
-  student3
-)
-for student in "${students[@]}"; do
-p=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w16 | head -n1)
-useradd -m $student
-echo $p | passwd $student --stdin
-echo "${student} ${p}" >> passwords.txt
-done
+  student65555
+  student1121
+      )
+      for student in "${students[@]}"; do
+              p=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w16 | head -n1)
+              useradd -m $student
+              (echo "$p"; echo "$p") | passwd $student
+              usermod -a -G rt $student
+              echo "${student} ${p}" >> passwords.txt
+      done
