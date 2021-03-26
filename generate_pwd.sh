811c1f9030091f5a156085c047d033da50e519c6
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-03-26 11:32:32 +0300

    Update generate_pwd.sh

--- generate_pwd.sh
+++ generate_pwd.sh
@@ -5,7 +5,7 @@ students=(
 )
 for student in "${students[@]}"; do
 p=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w16 | head -n1)
-useradd -m $student -p $p
+useradd -m $student
 echo $p | passwd $student --stdin
 echo "${student} ${p}" >> passwords.txt
 done
