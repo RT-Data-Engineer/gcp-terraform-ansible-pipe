c5cbbe74d7254f7eb98243eadd0577e0f805a522
asborsiov <70656108+asborsiov@users.noreply.github.com>
2021-04-11 19:39:57 +0300

    Update create-instances.tf

--- greenplum/create-instances.tf
+++ greenplum/create-instances.tf
@@ -25,7 +25,7 @@ resource "google_compute_instance" "greenplum" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
   }
 
